e4f1e1fdd02a8f85a5b90b18d7a796ae8fbca917
Wojciech Żywno <w.zywno@gmail.com>
2016-05-25 11:29:38 +0200

    update

--- RCTAutoComplete.ios.js
+++ RCTAutoComplete.ios.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-var { requireNativeComponent } = require('react');
+
+import {requireNativeComponent} from 'react-native';
 
 var NativeAutoComplete = requireNativeComponent('RCTAutoComplete', null);
 
